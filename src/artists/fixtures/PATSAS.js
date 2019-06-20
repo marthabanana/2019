1340e7cfae303a6e040c242ed6146c46cf13942d
marthabanana <marthag@afrobananarepublic.com>
2019-06-20 08:59:18 -0700

    Create PATSAS.js

---
+++ src/artists/fixtures/PATSAS.js
@@ -0,0 +1,19 @@
+import image from '../images/PATSAS.jpg'
+
+export const url = 'http://www.facebook.com/djpatsas'
+export const photo = image
+
+// Markdown fields
+export const name = 'PATSAS *(DJ SET)*'
+export const genres = '*HOUSE* *DISCO*'
+export const text = `
+Patsas loves to make people dance. And as the founder of Brandy Sour – event organiser and music curator extraordinaire – he really does know good music. As a DJ, Patsas takes his audiences on a journey through the more obscure, weird and wonderful end of the house and disco music spectrum.
+`.trim()
+
+export default {
+  name,
+  genres,
+  url,
+  photo,
+  text,
+}
