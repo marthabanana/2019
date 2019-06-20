ed097be89b8a46e9b5c32e1c5a5cc53e83117e35
marthabanana <marthag@afrobananarepublic.com>
2019-06-20 09:01:33 -0700

    Create NOK.js

---
+++ src/artists/fixtures/NOK.js
@@ -0,0 +1,19 @@
+import image from '../images/NOK.jpg'
+
+export const url = 'https://www.mixcloud.com/n-o-k/'
+export const photo = image
+
+// Markdown fields
+export const name = 'n.o.k *(DJ SET)*'
+export const genres = '*FUNK* *AFRO-SYNTH* *DISCO* *HOUSE*'
+export const text = `
+NOK is a real smooth groover and the island is paying attention. Making her DJ debut for Brandy Sour last summer, her sultry sound travels through the realms of soul, jazz, blues and funk. And festival vibes will be flowing with her AfroBanana set, combining beats from the African continent, stirred in with some global funk and disco.
+`.trim()
+
+export default {
+  name,
+  genres,
+  url,
+  photo,
+  text,
+}
