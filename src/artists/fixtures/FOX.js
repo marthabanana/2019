31c808b6644db1c74ef2a25c847ce0172fcdf9cc
marthabanana <marthag@afrobananarepublic.com>
2019-06-20 09:09:26 -0700

    Create FOX.js

---
+++ src/artists/fixtures/FOX.js
@@ -0,0 +1,19 @@
+import image from '../images/FOX.jpg'
+
+export const url = ''
+export const photo = image
+
+// Markdown fields
+export const name = 'MR. FOX *(DJ SET)*'
+export const genres = '*HOUSE* *DISCO* *TECHNO*'
+export const text = `
+A major player on the underground music scene in Cyprus Mr. Fox is not only a producer, promoter, co-founder of Disco Dystopia events and owner of Sousami Bar, he is also a performer, Deejaying around the world from Berlin to Vietnam. His sound is eclectic, diverse and progressive and as an ABR veteran, he’s guaranteed to fill those dancefloors.
+`.trim()
+
+export default {
+  name,
+  genres,
+  url,
+  photo,
+  text,
+}
