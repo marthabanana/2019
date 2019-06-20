f5df3f0a325040004483fff6ea419b0dc59b7c3e
marthabanana <marthag@afrobananarepublic.com>
2019-06-20 07:26:36 -0700

    Create BrandySourRadio.js

---
+++ src/activities/fixtures/BrandySourRadio.js
@@ -0,0 +1,15 @@
+import image from '../images/BRANDYSOURRADIO.jpg'
+
+export const photo = image
+
+// Markdown fields
+export const name = 'BRANDY SOUR RADIO'
+export const text = `
+This year our festival radio will be curated by our dear friends over at the Brandy Sour collective. They’ll be inviting their favourite local DJ’s to share two-hour sets which will be combined with interviews from the other friends of the festival. And listen up for the Open Deck session which will be activated on Saturday afternoon.
+`.trim()
+
+export default {
+  name,
+  photo,
+  text,
+}
