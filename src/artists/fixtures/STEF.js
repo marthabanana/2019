fb6020f544f46e5a4eb1e38e4024b4a59a99bb61
marthabanana <marthag@afrobananarepublic.com>
2019-06-20 09:06:46 -0700

    Create STEF.js

---
+++ src/artists/fixtures/STEF.js
@@ -0,0 +1,19 @@
+import image from '../images/STEF.jpg'
+
+export const url = ''
+export const photo = image
+
+// Markdown fields
+export const name = 'STEF *(DJ SET)*'
+export const genres = '*HIP HOP* *FUNK* *JAZZ* *ROCK* *ROCK* *ELECTRO* *ETHNIC*'
+export const text = `
+An all-round crowd pleaser, DJ Stef is a regular on the New Division decks and can also be found spinning his slick sounds around the capital. Allowing his music to flow according to the vibes of the night, DJ Stef handpicks sounds from the spectrum of hip-hop, funk, jazz, ethnic, alternative rock, disco and low temp electronica.
+`.trim()
+
+export default {
+  name,
+  genres,
+  url,
+  photo,
+  text,
+}
