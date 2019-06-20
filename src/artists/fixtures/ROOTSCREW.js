f93294149be652a55ab7f9339445ce77b734acb7
marthabanana <marthag@afrobananarepublic.com>
2019-06-20 09:18:50 -0700

    Create ROOTSCREW.js

---
+++ src/artists/fixtures/ROOTSCREW.js
@@ -0,0 +1,19 @@
+import image from '../images/ROOTSCREW.jpg'
+
+export const url = 'https://soundcloud.com/roots-renegade'
+export const photo = image
+
+// Markdown fields
+export const name = 'ROOTS CREW *(DJ SET)*'
+export const genres = '*ROOTS REGGAE*'
+export const text = `
+With a 19-year-long reputation for uplifting the crowds with their top-quality music and messages of human rights, freedom and equality, Roots Crew are the leaders on the reggae scene in Cyprus, bringing sounds based on the original principles of the Reggae sound systems and highly influenced by the underground culture of the90'sin the UK.
+`.trim()
+
+export default {
+  name,
+  genres,
+  url,
+  photo,
+  text,
+}
