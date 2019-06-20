a36aa11e2ad90ef47172ef1385757d14765e7510
marthabanana <marthag@afrobananarepublic.com>
2019-06-20 07:36:59 -0700

    Create MUSICALJOURNEY.js

---
+++ src/activities/fixtures/MUSICALJOURNEY.js
@@ -0,0 +1,18 @@
+import image from '../images/MUSICALJOURNEY.jpg'
+
+export const photo = image
+
+// Markdown fields
+export const name = 'MUSICAL JOURNEY OF DISCOVERY'
+export const text = `
+Give your little ones a lesson they will never forget, exploring the world through musical instruments. Experienced instructor, Anastasia Nicolaidou will guide participants on a musical journey of discovery, introducing them to new melodies, beats and rhythms from around the world, and inspiring creative expression and cultural curiosity along the way. 
+**Capacity:** 25 participants
+**Duration:** 1 hour
+**Age Group:**  Children aged 7 - 10 yrs
+`.trim()
+
+export default {
+  name,
+  photo,
+  text,
+}
