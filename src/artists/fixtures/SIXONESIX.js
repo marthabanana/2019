2bd53662c36cf58243c75aa36c13071d4b2ba138
marthabanana <marthag@afrobananarepublic.com>
2019-06-20 09:11:31 -0700

    Create SIXONESIX.js

---
+++ src/artists/fixtures/SIXONESIX.js
@@ -0,0 +1,19 @@
+import image from '../images/SIXONESIX.jpg'
+
+export const url = ''
+export const photo = image
+
+// Markdown fields
+export const name = 'SIXONESIX *(DJ SET)*'
+export const genres = '*HOUSE* *DISCO* '
+export const text = `
+His unique voice shines through in signature post punk, disco and house music DJ sets, which effortlessly blend world music with distorted guitar sounds. And Constantinos – a.k.a Sixonesix – is in high demand, spinning tracks at venues and parties around the island and now letting his beats loose on the festival stage.
+`.trim()
+
+export default {
+  name,
+  genres,
+  url,
+  photo,
+  text,
+}
