ffceb82132716b2f7718c6849a7362b24a189e6b
marthabanana <marthag@afrobananarepublic.com>
2019-06-21 00:32:13 -0700

    Create KUTIMAN.js

---
+++ src/artists/fixtures/KUTIMAN.js
@@ -0,0 +1,19 @@
+import image from '../images/KUTIMAN.jpg'
+
+export const url = 'https://youtu.be/dPbAcaBmaUo'
+export const photo = image
+
+// Markdown fields
+export const name = 'KUTIMAN *(IL)* *(LIVE)*'
+export const genres = '*PSYCHEDELIC FUNK* *AFROBEAT* *COSMIC JAZZ*'
+export const text = `
+We’re pretty excited to announce headline act Kutiman – the psychedelic space funk architects straight out of Tel Aviv who have been kicking up a storm on the music scene for over a decade. Pioneering a new hybrid of visual and audio language, Kutiman’s creations and visions come to an inevitable climax on stage with his live outlet, The Kutiman Orchestra.
+`.trim()
+
+export default {
+  name,
+  genres,
+  url,
+  photo,
+  text,
+}
