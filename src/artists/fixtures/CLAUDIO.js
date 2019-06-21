0c14eb7db5fb05db8adb3695b7cacc5a2a5ca413
marthabanana <marthag@afrobananarepublic.com>
2019-06-21 01:14:57 -0700

    Update CLAUDIO.js

--- src/artists/fixtures/CLAUDIO.js
+++ src/artists/fixtures/CLAUDIO.js
@@ -7,7 +7,7 @@ export const photo = image
 export const name = 'CLAUDIO *(DJ SET)*'
 export const genres = '*WORLD* *JAZZ* *BLUES* *FUNK FUSION*'
 export const text = `
-Co-organiser of the popular Nostalgia Parties and regular DJ at some of Cyprus’ coolest venues and parties, Claudio’s eclectic taste spans blues, rock, jazz and soul, to disco, electro, hip hop and drum & bass. Wake up to his smooth sounds on Saturday morning, with a soothing Vinyl-only set fusing afro, jazz, blues and funk.
+Co-organiser of the popular Nostalgia Parties and regular DJ at some of Cyprus’ coolest venues and parties, Claudio’s eclectic taste spans blues, rock, jazz and soul, to disco, electro, hip hop and drum & bass. Dance to his smooth sounds on Saturday, with a Vinyl-only set fusing afro, jazz, blues and funk.
 `.trim()
 
 export default {
