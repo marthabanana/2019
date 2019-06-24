258c662194f3d732f51e68bb1d50b8eaee78cdad
marthabanana <marthag@afrobananarepublic.com>
2019-06-24 06:34:32 -0700

    Update ACROYOGA.js

--- src/activities/fixtures/ACROYOGA.js
+++ src/activities/fixtures/ACROYOGA.js
@@ -5,7 +5,7 @@ export const photo = image
 // Markdown fields
 export const name = 'ACROYOGA'
 export const text = `
-AcroYoga International Teacher and ambassador for Cyprus, Skevi Chrysou (Namaste Yoga Home Studio - https://www.facebook.com/Yogastudio2016/) invites you to dissolve your fears and tap into new and infinite possibilities of communication, trust and union, with a unique AcroYoga class, combining yoga, acrobatics and healing Thai massage. Suitable for all levels, you’ll be surprised by what you can do in this exciting session.
+AcroYoga International Teacher and ambassador for Cyprus, Skevi Chrysou invites you to dissolve your fears and tap into new and infinite possibilities of communication, trust and union, with a unique AcroYoga class, combining yoga, acrobatics and healing Thai massage. Suitable for all levels, you’ll be surprised by what you can do in this exciting session. Namaste Yoga Home Studio - https://www.facebook.com/Yogastudio2016/
 
 **Capacity:** 30 participants
 
