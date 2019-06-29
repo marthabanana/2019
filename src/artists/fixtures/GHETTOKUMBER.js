ed8ed20ea6645cabbde4919519a9c4cf40ce89d0
marthabanana <marthag@afrobananarepublic.com>
2019-06-29 14:26:05 +0300

    Update and rename GHETTOKUMBE.js to GHETTOKUMBER.js

--- src/artists/fixtures/GHETTOKUMBER.js
+++ src/artists/fixtures/GHETTOKUMBER.js
@@ -4,7 +4,7 @@ export const url = 'https://youtu.be/AxK0igfWJhk'
 export const photo = image
 
 // Markdown fields
-export const name = 'GHETTO KUMBER *(CO)**(LIVE)*'
+export const name = 'GHETTO KUMBE *(CO)**(LIVE)*'
 export const genres = '*CARIBBEAN* *AFRO-HOUSE* *COLOMBIAN BEAT*'
 export const text = `
 Coming to Cyprus straight from Glastonbury Festival and Roskilde Festival, the explosive Ghetto Kumbé trio are preparing to ignite the AfroBanana dancefloor with the hypnotic power of their unique African house sound. Fusing their rhythmic Afro-Colombian roots with Caribbean and African rhythms and intense electronic beats, Ghetto Kumbé keeps the party rocking.
