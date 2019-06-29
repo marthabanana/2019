b847e6853a6207c7c64336c44616dd7ac5b2e6ce
marthabanana <marthag@afrobananarepublic.com>
2019-06-29 14:32:44 +0300

    Update SELFAWARENESS.js

--- src/activities/fixtures/SELFAWARENESS.js
+++ src/activities/fixtures/SELFAWARENESS.js
@@ -6,8 +6,11 @@ export const photo = image
 export const name = 'SELF AWARENESS WORKSHOP'
 export const text = `
 We could all do with some lessons in the art of self-awareness and mindfulness. And now you can, with this creative workshop where participants will use artistic materials such as clay, colours, paper and even things taken from the beautiful surrounding nature, to discover hidden parts of their character, through artistic expression.
+
 **Capacity:** 20 participants
+
 **Duration:** 1 hour
+
 **Age Group:**  Ages 16+ yrs
 `.trim()
 
