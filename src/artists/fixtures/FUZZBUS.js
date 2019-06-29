59f7e92b389be96d341f51475ba3ec0058180e5e
marthabanana <marthag@afrobananarepublic.com>
2019-06-29 14:13:40 +0300

    Update FUZZBUS.js

--- src/artists/fixtures/FUZZBUS.js
+++ src/artists/fixtures/FUZZBUS.js
@@ -4,7 +4,7 @@ export const url = ''
 export const photo = image
 
 // Markdown fields
-export const name = 'FUZZ BUS  *(CY)**(DJ SET)*'
+export const name = 'FUZZ BUS  *(CY)**(LIVE)*'
 export const genres = '*SOUL* *FUNK JAZZ*'
 export const text = `
 Fiercely energetic and compellingly unpredictable, the Fuzz Bus quartet are all about solid grooves and impromptu improvisation. New original material will be debuted at the festival this year, combining their own unique sound, with the band’s signature re-arrangements of known and unknown soul, funk and pop rock songs.
