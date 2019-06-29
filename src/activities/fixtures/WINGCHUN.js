bd8480bf60c6431a772102ee63ef3163e1ba2644
marthabanana <marthag@afrobananarepublic.com>
2019-06-29 14:31:59 +0300

    Update WINGCHUN.js

--- src/activities/fixtures/WINGCHUN.js
+++ src/activities/fixtures/WINGCHUN.js
@@ -6,8 +6,11 @@ export const photo = image
 export const name = 'WING CHUN'
 export const text = `
 It has roots in the traditional Southern Chinese Kung Fu of the Shaolin monks and now Wing Chun is coming to AfroBanana Festival. Instructor Aphrodite Nikolaou will share and teach some of the powerful movements and ancient philosophies behind this beautiful and mystical martial art, which is said to have been created by a woman. Girl power!
+
 **Capacity:** 20 participants
+
 **Duration:** 1 hour
+
 **Age Group:**  Children aged 15+ yrs
 `.trim()
 
