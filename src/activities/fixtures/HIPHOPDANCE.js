49feab2ee3dcb00da1a8f2d39d0758c3f7b022d5
marthabanana <marthag@afrobananarepublic.com>
2019-06-29 14:30:08 +0300

    Update HIPHOPDANCE.js

--- src/activities/fixtures/HIPHOPDANCE.js
+++ src/activities/fixtures/HIPHOPDANCE.js
@@ -6,8 +6,11 @@ export const photo = image
 export const name = 'HIP HOP DANCE WORKSHOP'
 export const text = `
 Nefeli 'sMash' Tsiouti of ProjectBreakalign wants to break it down with dance lovers, in a unique movement workshop focusing on teaching the foundations of hip-hop and breakdancing and showing participants how to use these foundations to express their own creativity in a unique way. There are no rules, and this is not a typical dance class!
+
 **Capacity:** 20 participants
+
 **Duration:** 1 hour
+
 **Age Group:** Children aged 15+ yrs
 `.trim()
 
